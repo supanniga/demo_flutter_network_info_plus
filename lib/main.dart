e6b58ff90ad86f625c70c34092f0fb88a72de318
GEC\supanniga.oga <supanniga.oga@gec.com>
2022-01-16 22:57:21 +0700

    add network_info_plus in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,22 @@
+import 'dart:async';
+import 'dart:developer' as developer;
+import 'dart:io';
+
+import 'package:network_info_plus/network_info_plus.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+// Sets a platform override for desktop to avoid exceptions. See
+// https://flutter.dev/desktop#target-platform-override for more info.
+void _enablePlatformOverrideForDesktop() {
+  if (!kIsWeb && (Platform.isWindows || Platform.isLinux)) {
+    debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
+  }
+}
 
 void main() {
+  _enablePlatformOverrideForDesktop();
   runApp(const MyApp());
 }
 
@@ -29,12 +45,13 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  String _connectionStatus = 'Unknown';
+  final NetworkInfo _networkInfo = NetworkInfo();
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    _initNetworkInfo();
   }
 
   @override
@@ -43,25 +60,109 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      body: Center(child: Text('Connection Status: $_connectionStatus')),
     );
+  }
+
+  Future<void> _initNetworkInfo() async {
+    String? wifiName,
+        wifiBSSID,
+        wifiIPv4,
+        wifiIPv6,
+        wifiGatewayIP,
+        wifiBroadcast,
+        wifiSubmask;
+
+    try {
+      if (!kIsWeb && Platform.isIOS) {
+        var status = await _networkInfo.getLocationServiceAuthorization();
+        if (status == LocationAuthorizationStatus.notDetermined) {
+          status = await _networkInfo.requestLocationServiceAuthorization();
+        }
+        if (status == LocationAuthorizationStatus.authorizedAlways ||
+            status == LocationAuthorizationStatus.authorizedWhenInUse) {
+          wifiName = await _networkInfo.getWifiName();
+        } else {
+          wifiName = await _networkInfo.getWifiName();
+        }
+      } else {
+        wifiName = await _networkInfo.getWifiName();
+      }
+    } on PlatformException catch (e) {
+      developer.log('Failed to get Wifi Name', error: e);
+      wifiName = 'Failed to get Wifi Name';
+    }
+
+    try {
+      if (!kIsWeb && Platform.isIOS) {
+        var status = await _networkInfo.getLocationServiceAuthorization();
+        if (status == LocationAuthorizationStatus.notDetermined) {
+          status = await _networkInfo.requestLocationServiceAuthorization();
+        }
+        if (status == LocationAuthorizationStatus.authorizedAlways ||
+            status == LocationAuthorizationStatus.authorizedWhenInUse) {
+          wifiBSSID = await _networkInfo.getWifiBSSID();
+        } else {
+          wifiBSSID = await _networkInfo.getWifiBSSID();
+        }
+      } else {
+        wifiBSSID = await _networkInfo.getWifiBSSID();
+      }
+    } on PlatformException catch (e) {
+      developer.log('Failed to get Wifi BSSID', error: e);
+      wifiBSSID = 'Failed to get Wifi BSSID';
+    }
+
+    try {
+      wifiIPv4 = await _networkInfo.getWifiIP();
+    } on PlatformException catch (e) {
+      developer.log('Failed to get Wifi IPv4', error: e);
+      wifiIPv4 = 'Failed to get Wifi IPv4';
+    }
+
+    try {
+      wifiIPv6 = await _networkInfo.getWifiIPv6();
+    } on PlatformException catch (e) {
+      developer.log('Failed to get Wifi IPv6', error: e);
+      wifiIPv6 = 'Failed to get Wifi IPv6';
+    }
+
+    try {
+      wifiSubmask = await _networkInfo.getWifiSubmask();
+    } on PlatformException catch (e) {
+      developer.log('Failed to get Wifi submask address', error: e);
+      wifiSubmask = 'Failed to get Wifi submask address';
+    }
+
+    try {
+      wifiBroadcast = await _networkInfo.getWifiBroadcast();
+    } on PlatformException catch (e) {
+      developer.log('Failed to get Wifi broadcast', error: e);
+      wifiBroadcast = 'Failed to get Wifi broadcast';
+    }
+
+    try {
+      wifiGatewayIP = await _networkInfo.getWifiGatewayIP();
+    } on PlatformException catch (e) {
+      developer.log('Failed to get Wifi gateway address', error: e);
+      wifiGatewayIP = 'Failed to get Wifi gateway address';
+    }
+
+    try {
+      wifiSubmask = await _networkInfo.getWifiSubmask();
+    } on PlatformException catch (e) {
+      developer.log('Failed to get Wifi submask', error: e);
+      wifiSubmask = 'Failed to get Wifi submask';
+    }
+
+    setState(() {
+      _connectionStatus = 'Wifi Name: $wifiName\n'
+          'Wifi BSSID: $wifiBSSID\n'
+          'Wifi IPv4: $wifiIPv4\n'
+          'Wifi IPv6: $wifiIPv6\n'
+          'Wifi Broadcast: $wifiBroadcast\n'
+          'Wifi Gateway: $wifiGatewayIP\n'
+          'Wifi Submask: $wifiSubmask\n';
+    });
   }
 }
